cf66f7e2bb504c12ea0d16dca3cbdd8203dbc40e
James Walker <james.chunho@gmail.com>
2024-07-10 01:47:33 +0800

    package wip

--- src/repo.rs
+++ src/repo.rs
@@ -1,6 +1,9 @@
 use anyhow::Result;
 use ini::Ini;
-use std::path::{Path, PathBuf};
+use std::{
+    fs::read_to_string,
+    path::{Path, PathBuf},
+};
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -35,5 +38,53 @@ impl Repo {
             author: author.into(),
             link_pattern: link_pattern.into(),
         })
+    }
+}
+
+#[derive(Debug)]
+pub(crate) struct Package {
+    name: String,
+    r#type: String,
+    desc: Option<String>,
+}
+
+impl Package {
+    pub(crate) fn read(dir: &Path) -> Result<Self> {
+        let config_path = dir.join("package.ini");
+        let ini = Ini::load_from_file(config_path)?;
+
+        let section = ini
+            .section(Some("package"))
+            .ok_or(ConfigSectionMissing("package"))?;
+
+        let r#type = section.get("type").ok_or(ConfigKeyMissing("type"))?;
+        let name = section
+            .get("name")
+            .map(|x| x.into())
+            // default to directory name
+            .or(dir.file_name().map(|x| x.to_string_lossy()))
+            // if directory name is somehow missing, complain about config
+            .ok_or(ConfigKeyMissing("name"))?;
+        let desc = Self::read_description(dir)?;
+
+        Ok(Self {
+            r#type: r#type.into(),
+            name: name.into(),
+            desc,
+        })
+    }
+
+    fn read_description(dir: &Path) -> Result<Option<String>> {
+        let rtf_path = dir.join("README.rtf");
+        if rtf_path.exists() {
+            return Ok(Some(read_to_string(rtf_path)?));
+        }
+
+        let md_path = dir.join("README.md");
+        if md_path.exists() {
+            todo!("implement pandoc handling")
+        }
+
+        Ok(None)
     }
 }
