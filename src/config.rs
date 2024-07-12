e4fb5597c41dc6d34dee4aa8b8ebb4010a59e304
James Walker <james.chunho@gmail.com>
2024-07-12 20:12:16 +0800

    Support for entrypoints

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,4 @@
-use std::{path::PathBuf, str::FromStr};
+use std::{collections::HashMap, path::PathBuf, str::FromStr};
 
 use chrono::{DateTime, Utc};
 use relative_path::RelativePathBuf;
@@ -86,6 +86,67 @@ impl<'de> Deserialize<'de> for PackageType {
     }
 }
 
+#[derive(PartialEq, Eq, Hash, Debug)]
+pub(crate) enum ActionListSection {
+    MainSection,                // main
+    MIDIEditorSection,          // midi_editor
+    MIDIInlineEditorSection,    // midi_inlineeditor
+    MIDIEventListEditorSection, // midi_eventlisteditor
+    MediaExplorerSection,       // mediaexplorer
+}
+
+#[derive(Error, Debug)]
+#[error("invalid action list section: {0}")]
+pub(crate) struct InvalidActionListSection(String);
+
+impl FromStr for ActionListSection {
+    type Err = InvalidActionListSection;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "main" => Ok(Self::MainSection),
+            "midi_editor" => Ok(Self::MIDIEditorSection),
+            "midi_inlineeditor" => Ok(Self::MIDIInlineEditorSection),
+            "midi_eventlisteditor" => Ok(Self::MIDIEventListEditorSection),
+            "mediaexplorer" => Ok(Self::MediaExplorerSection),
+            _ => Err(InvalidActionListSection(s.into())),
+        }
+    }
+}
+
+impl From<&ActionListSection> for &str {
+    fn from(value: &ActionListSection) -> Self {
+        match value {
+            ActionListSection::MainSection => "main",
+            ActionListSection::MIDIEditorSection => "midi_editor",
+            ActionListSection::MIDIInlineEditorSection => "midi_inlineeditor",
+            ActionListSection::MIDIEventListEditorSection => "midi_eventlisteditor",
+            ActionListSection::MediaExplorerSection => "mediaexplorer",
+        }
+    }
+}
+
+impl Serialize for ActionListSection {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_str(self.into())
+    }
+}
+
+impl<'de> Deserialize<'de> for ActionListSection {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        // let x = String::deserialize(deserializer)?;
+        let text = String::deserialize(deserializer)?;
+        text.parse::<ActionListSection>()
+            .map_err(|e| serde::de::Error::custom(e))
+    }
+}
+
 #[derive(Serialize, Deserialize)]
 pub(crate) struct RepositoryConfig {
     pub(crate) identifier: Option<String>,
@@ -100,9 +161,11 @@ pub(crate) struct PackageConfig {
     pub(crate) r#type: PackageType,
     pub(crate) identifier: Option<String>,
     pub(crate) author: Option<String>,
+    pub(crate) entrypoints: Option<HashMap<ActionListSection, Vec<RelativePathBuf>>>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub(crate) struct VersionConfig {
     pub(crate) time: DateTime<Utc>,
+    pub(crate) entrypoints: Option<HashMap<ActionListSection, Vec<RelativePathBuf>>>,
 }
