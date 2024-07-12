c5ccdbab2824a84f4bedf134fddc2a311617a0ee
James Walker <james.chunho@gmail.com>
2024-07-12 16:22:02 +0800

    new version module

--- src/version.rs
+++ src/version.rs
@@ -6,72 +6,56 @@ use thiserror::Error;
 #[error("unable to parse this version string: {0}")]
 pub(crate) struct UnknownVersionFormat(String);
 
-pub(crate) struct Version {
-    a: u32,
-    b: u32,
-    c: u32,
-}
+pub(crate) fn increment_version(text: &str) -> Result<String, UnknownVersionFormat> {
+    let text = text.to_string();
 
-impl Version {
-    pub(crate) fn increment(&self) -> Self {
-        Self {
-            a: self.a,
-            b: self.b,
-            c: self.c + 1,
+    let suffix = {
+        let mut suffix = String::new();
+        for c in text.chars().rev() {
+            // if found non-digit char, stop the loop
+            if c.is_digit(10) {
+                suffix.push(c);
+            } else {
+                break;
+            }
         }
-    }
-}
-
-impl Default for Version {
-    fn default() -> Self {
-        Self { a: 0, b: 0, c: 1 }
-    }
-}
-
-impl FromStr for Version {
-    type Err = UnknownVersionFormat;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut parts = s.split(".");
-
-        let a = {
-            let text = parts
-                .next()
-                .ok_or_else(|| UnknownVersionFormat(s.to_string()))?;
-            let num = text
-                .parse::<u32>()
-                .map_err(|_| UnknownVersionFormat(s.to_string()))?;
-            Ok(num)
-        }?;
-        let b = {
-            let text = parts
-                .next()
-                .ok_or_else(|| UnknownVersionFormat(s.to_string()))?;
-            let num = text
-                .parse::<u32>()
-                .map_err(|_| UnknownVersionFormat(s.to_string()))?;
-            Ok(num)
-        }?;
-        let c = {
-            let text = parts
-                .next()
-                .ok_or_else(|| UnknownVersionFormat(s.to_string()))?;
-            let num = text
-                .parse::<u32>()
-                .map_err(|_| UnknownVersionFormat(s.to_string()))?;
-            Ok(num)
-        }?;
-
-        if parts.next().is_some() {
-            return Err(UnknownVersionFormat(s.to_string()));
+        if suffix.is_empty() {
+            return Err(UnknownVersionFormat(text.into()));
         }
+        suffix = suffix.chars().rev().collect();
+        Ok(suffix)
+    }?;
 
-        Ok(Self { a, b, c })
-    }
+    // Parse the suffix to an integer
+    let incremented_suffix = suffix.parse::<u32>().unwrap() + 1;
+
+    // Create the new version string
+    let prefix_len = text.len() - suffix.len();
+
+    Ok(format!("{}{}", &text[..prefix_len], incremented_suffix))
 }
 
-impl From<&Version> for String {
-    fn from(ver: &Version) -> Self {
-        format!("{}.{}.{}", ver.a, ver.b, ver.c)
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_01() {
+        let result = increment_version("0.1.15").unwrap();
+        let expected = "0.1.16";
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_02() {
+        let result = increment_version("0.1").unwrap();
+        let expected = "0.2";
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_03() {
+        let result = increment_version("0.1a");
+        assert!(result.is_err());
     }
 }
