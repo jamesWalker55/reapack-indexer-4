1f063a3e64fda16a9bbf8e39e78e12411d8e7de1
James Walker <james.chunho@gmail.com>
2024-07-13 21:25:06 +0800

    fix type name serialising

--- src/config.rs
+++ src/config.rs
@@ -158,6 +158,7 @@ pub(crate) struct RepositoryConfig {
 pub(crate) struct PackageConfig {
     pub(crate) name: Option<String>,
     pub(crate) category: RelativePathBuf,
+    #[serde(rename = "type")]
     pub(crate) pkg_type: PackageType,
     pub(crate) identifier: Option<String>,
     pub(crate) author: Option<String>,
